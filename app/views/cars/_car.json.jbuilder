58275f3a986337ecb5962ad5fc63dbac862ce0d5
jpashley1 <jpashley1@gmail.com>
2024-09-30 18:49:40 -0400

    Created cars database and restful routes

---
+++ app/views/cars/_car.json.jbuilder
@@ -0,0 +1,6 @@
+json.id car.id
+json.make car.make
+json.model car.model
+json.year car.year
+json.created_at car.created_at
+json.updated_at car.updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
+  get "/cars" => "cars#index" 
+  get "/cars/:id" => "cars#show" 
+  post "/cars" => "cars#create" 
+  patch "/cars/:id" => "cars#update" 
+  delete "/cars/:id" => "cars#destroy"
+
+
+  post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
+
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
